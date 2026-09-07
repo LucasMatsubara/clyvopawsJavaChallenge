7b5eec3cbbf7f7757684548bd234d4ad74a3a8f4
LucasMatsubara <lucasmatsubara11@gmail.com>
2026-09-07 18:44:26 -0300

    Implementando as configurações de acesso e sessões por meio do JWT.

--- src/main/java/br/com/fiap/clyvopaws/ClyvovetApplication.java
+++ src/main/java/br/com/fiap/clyvopaws/ClyvovetApplication.java
@@ -2,10 +2,12 @@ package br.com.fiap.clyvopaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@ConfigurationPropertiesScan
 public class ClyvovetApplication {
 
 	public static void main(String[] args) {
