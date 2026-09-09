48480e94e67b65545967a00c617f800f21f5069b
LucasMatsubara <lucasmatsubara11@gmail.com>
2026-09-09 02:43:41 -0300

    Implementando autorização via Token no Swagger.

--- src/main/java/br/com/fiap/clyvopaws/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/clyvopaws/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package br.com.fiap.clyvopaws.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI clyvoVetOpenAPI() {
@@ -17,6 +22,13 @@ public class SwaggerConfig {
                         .description("API RESTful para o Sistema de Cuidado Contínuo e Plano de Alta Inteligente da Clyvo Vet.")
                         .version("v1.0.0")
                         .contact(new Contact()
-                                .name("Equipe [LevelUp]")));
+                                .name("Equipe [LevelUp]")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
